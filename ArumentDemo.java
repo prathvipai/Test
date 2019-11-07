72791dc9cac4413635005549737949f7cd96ff13
prathvipai <56383411+prathvipai@users.noreply.github.com>
2019-11-07 11:15:32 +0530

    Add files via upload

---
+++ ArumentDemo.java
@@ -0,0 +1,15 @@
+class ArumentDemo{
+
+public static void main(String[] args){
+System.out.println("the following arguments are passed during runtime");
+System.out.println(args[0]);
+System.out.println(args[1]);
+System.out.println(args[2]);
+int first,second,sum;
+System.out.println("Testing...");
+first=Integer.parseInt(args[3]);
+second=Integer.parseInt(args[4]);
+sum=first+second;
+System.out.println("the of two num is sum"+sum);
+
+}}
